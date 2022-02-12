7be7ad0f816ffa74ef4c5745e0df31e8820c7b30
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-02-12 17:06:10 +0400

    updates to abstractions IOC

--- HaleyAbstractions/MVVM/Interfaces/IContainerFactory.cs
+++ HaleyAbstractions/MVVM/Interfaces/IContainerFactory.cs
@@ -5,9 +5,10 @@ namespace Haley.Abstractions
     public interface IContainerFactory
     {
         string Id { get; }
+        bool Initiate();
         IServiceProvider Services { get;  }
         IControlContainer Controls { get; } 
         IWindowContainer Windows { get; }
-        bool RegisterSelf();
+        IBaseContainer GetDI();
     }
 }
