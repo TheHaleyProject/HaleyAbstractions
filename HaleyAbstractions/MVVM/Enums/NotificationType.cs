233e5583af1187b10609d6221e794db723ed6665
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-02-12 04:25:16 +0400

    Moved dialogservice abstractions

---
+++ HaleyAbstractions/MVVM/Enums/NotificationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Haley.Enums
+{
+    //This is an internal enum to switch different dialog modes.
+    public enum DisplayType
+    {
+     ShowInfo,
+     GetInput,
+     GetConfirmation,
+     ToastInfo,
+     ContainerView,
+     CustomView
+    }
+}
---
+++ HaleyAbstractions/MVVM/Interfaces/INotification.cs
@@ -0,0 +1,23 @@
+﻿using Haley.Enums;
+
+namespace Haley.Abstractions
+{
+    public interface INotification
+    {
+        #region Properties
+        object ContainerViewModel { get; }
+        string Id { get;  }
+        DisplayType Type { get; set; }
+        string Message { get; set; }
+        NotificationIcon NotificationIcon { get; set; }
+        bool ShowNotificationIcon { get; set; }
+        string UserInput { get; set; }
+        bool? DialogResult { get; set; }
+        string AppName { get; set; }
+
+        bool AutoClose { get; set; }
+        int CountDown { get; set; }
+        double GlowRadius { get; set; }
+        #endregion
+    }
+}
