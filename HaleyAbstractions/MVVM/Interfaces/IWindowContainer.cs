c940dbd8f45cb792bcce3925aa63702fc680d70e
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-02-16 14:13:41 +0400

    updates

--- HaleyAbstractions/MVVM/Interfaces/IWindowContainer.cs
+++ HaleyAbstractions/MVVM/Interfaces/IWindowContainer.cs
@@ -7,7 +7,7 @@ namespace Haley.Abstractions
     {
         #region ShowDialog Methods
         bool? ShowDialog<VMType>(VMType InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered) where VMType : class, IHaleyVM;
-        bool? ShowDialog<ViewType>(ResolveMode resolve_mode = ResolveMode.AsRegistered) where ViewType : class;
+        bool? ShowViewDialog<ViewType>(ResolveMode resolve_mode = ResolveMode.AsRegistered) where ViewType : class;
         bool? ShowDialog(string key, object InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered);
         bool? ShowDialog(Enum key, object InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered);
 
@@ -15,7 +15,7 @@ namespace Haley.Abstractions
 
         #region Show Methods
         void Show<VMType>(VMType InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered) where VMType : class, IHaleyVM;
-        void Show<ViewType>(ResolveMode resolve_mode = ResolveMode.AsRegistered) where ViewType : class;
+        void ShowView<ViewType>(ResolveMode resolve_mode = ResolveMode.AsRegistered) where ViewType : class;
         void Show(string key, object InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered);
         void Show(Enum key, object InputViewModel = null, ResolveMode resolve_mode = ResolveMode.AsRegistered);
         #endregion
