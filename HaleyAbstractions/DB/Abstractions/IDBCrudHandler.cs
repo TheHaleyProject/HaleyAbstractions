dbaa30b6b2c4c05bb956e601b464a55ee560b595
LingamHPZ4 <rmsmech@gmail.com>
2025-03-13 13:34:19 +0400

    some changes

--- HaleyAbstractions/DB/Abstractions/IDBCrudHandler.cs
+++ HaleyAbstractions/DB/Abstractions/IDBCrudHandler.cs
@@ -7,7 +7,10 @@ using System;
 namespace Haley.Abstractions {
     public interface IDBCrudHandler {
         Task<object> Read(IDBInput input, params (string key, object value)[] parameters);
+        Task<object> Read(string adapterkey,string query, params (string key, object value)[] parameters);
         Task<object> Scalar(IDBInput input, params (string key, object value)[] parameters);
+        Task<object> Scalar(string adapterkey, string query, params (string key, object value)[] parameters);
         Task<object> NonQuery(IDBInput input,  params (string key, object value)[] parameters);
+        Task<object> NonQuery(string adapterkey, string query, params (string key, object value)[] parameters);
     }
 }
