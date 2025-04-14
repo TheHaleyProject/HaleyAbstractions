c58df81f4b74181b9ec606fcd7e43939aafd2018
LingamHPZ4 <rmsmech@gmail.com>
2025-04-14 14:31:29 +0400

    minor changes

---
+++ HaleyAbstractions/Extensions/Enums/DataSizeDecimal.cs
@@ -0,0 +1,21 @@
+﻿namespace Haley.Enums {
+    public enum DataSizeDecimal {
+    B = 0,
+    KB =1,
+    MB =2,
+    GB =3,
+    TB =4,
+    PB =5,
+    EB =6
+    }
+
+    public enum DataSizeBinary {
+        B =0,
+        KiB =1,
+        MiB =2,
+        GiB =3,
+        TiB =4,
+        PiB =5,
+        EiB=6
+    }
+}
