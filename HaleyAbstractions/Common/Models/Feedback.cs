f6ce0d3e363f2075c496275b179ab8cbadc0a621
LingamHPZ4 <rmsmech@gmail.com>
2025-07-01 10:42:25 +0400

    Changes

--- HaleyAbstractions/Common/Models/Feedback.cs
+++ HaleyAbstractions/Common/Models/Feedback.cs
@@ -5,6 +5,29 @@ namespace Haley.Models {
         public bool Status { get; set; }
         public string Message { get; set; }
         public object Result { get; set; }
+
+        public Feedback SetStatus(bool status) {
+            Status = status;
+            return this;
+        }
+
+        public Feedback SetMessage(string message) {
+            Message = message;
+            return this;
+        }
+
+        public Feedback SetResult(object result) {
+            Result = result;
+            return this;
+        }
+
+        public Feedback Clear() {
+            Status = false;
+            Result = null;
+            Message = string.Empty;
+            return this;
+        }
+
         public override string ToString() {
             return $@"{Status} : {Message} -- {Result}";
         }
