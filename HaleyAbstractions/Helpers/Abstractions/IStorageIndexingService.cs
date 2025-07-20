d116766c71e163ab1416b5118abe0f6ee77860b7
LingamHPZ4 <rmsmech@gmail.com>
2025-07-20 15:45:34 +0400

    commit

---
+++ HaleyAbstractions/Helpers/Abstractions/IStorageIndexingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Haley.Abstractions {
+    public interface IStorageIndexingService {
+        IFeedback GetFile();
+        bool UpsertClient();
+        bool UpsertDirectory();
+        bool UpsertFile();
+    }
+}
