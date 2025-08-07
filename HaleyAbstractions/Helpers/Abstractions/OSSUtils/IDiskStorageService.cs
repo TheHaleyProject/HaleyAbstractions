e2e8135afc7a4f00b484ba4fd3947e1de1fb4760
LingamHPZ4 <rmsmech@gmail.com>
2025-08-07 09:33:18 +0400

    Basics covered

--- HaleyAbstractions/Helpers/Abstractions/OSSUtils/IDiskStorageService.cs
+++ HaleyAbstractions/Helpers/Abstractions/OSSUtils/IDiskStorageService.cs
@@ -11,8 +11,7 @@ namespace Haley.Abstractions {
         Task<IFeedback> RegisterClient(IOSSControlled name, string password = null);
         Task<IFeedback> RegisterModule(string name, string client_name = null); //If a client is not registered, we register it against "Default"
         Task<IFeedback> RegisterModule(IOSSControlled input, IOSSControlled client); //If a client is not registered, we register it against "Default"
-        Task<IFeedback> RegisterWorkSpace(string name, string client_name = null,string module_name = null); //If a client is not registered, we register it against "Default"
-        Task<IFeedback> RegisterWorkSpace(string name, string client_name, string module_name, OSSControlMode content_control = OSSControlMode.None, OSSParseMode content_pmode = OSSParseMode.Parse); //If a client is not registered, we register it against "Default"
+        Task<IFeedback> RegisterWorkSpace(string name, string client_name=null, string module_name=null, OSSControlMode content_control = OSSControlMode.None, OSSParseMode content_pmode = OSSParseMode.Parse); //If a client is not registered, we register it against "Default"
         Task<IFeedback> RegisterWorkSpace(IOSSControlled input, IOSSControlled client,IOSSControlled module, OSSControlMode content_control = OSSControlMode.None, OSSParseMode content_pmode = OSSParseMode.Parse); //If a client is not registered, we register it against "Default"
         Task<IFeedback> AuthorizeClient(object clientInfo, object clientSecret);
         string GetStorageRoot();
