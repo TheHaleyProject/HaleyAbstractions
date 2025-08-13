1a386272f8cb2bce2186e7a4385b5d7915e8e116
LingamHPZ4 <rmsmech@gmail.com>
2025-08-13 09:28:28 +0400

    added writemode to appsettings

--- HaleyAbstractions/Helpers/Models/OSSConstants.cs
+++ HaleyAbstractions/Helpers/Models/OSSConstants.cs
@@ -6,10 +6,11 @@ namespace Haley.Models {
     public class OSSConstants {
         public const string DEFAULT_NAME = "default";
         public const string CONFIG_SOURCE = "OSSSource";
-        public const string CONFIG_DIRECTORIES = "OSSDirectories";
+        public const string CONFIG_INFO = "OSSInfo";
         public const string CONFIG_DIR_WIN = "win";
         public const string CONFIG_DIR_LINUX = "linux";
         public const string CONFIG_DIR_RESPONSEPATHMODE = "responsePathMode";
         public const string CONFIG_DEFADAPTER = "defadapter";
+        public const string CONFIG_WRITEMODE = "writemode";
     }
 }
