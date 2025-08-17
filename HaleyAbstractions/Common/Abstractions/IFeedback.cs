cdba54f6add76a2659bd513e391b1d0576b91300
LingamHPZ4 <rmsmech@gmail.com>
2025-08-17 19:26:32 +0400

    Updated

--- HaleyAbstractions/Common/Abstractions/IFeedback.cs
+++ HaleyAbstractions/Common/Abstractions/IFeedback.cs
@@ -1,7 +1,14 @@
-﻿namespace Haley.Abstractions {
-    public interface IFeedback {
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Haley.Abstractions {
+    public interface IFeedback : IFeedback<object> {
+       
+    }
+
+    public interface IFeedback<T>  {
         bool Status { get; set; }
         string Message { get; set; }
-        object Result { get; set; }
+        T Result { get; set; }
     }
 }
--- HaleyAbstractions/Common/Models/Feedback.cs
+++ HaleyAbstractions/Common/Models/Feedback.cs
@@ -1,11 +1,8 @@
 ﻿using Haley.Abstractions;
 
 namespace Haley.Models {
-    public class Feedback : IFeedback {
-        public bool Status { get; set; }
-        public string Message { get; set; }
-        public object Result { get; set; }
 
+    public class Feedback : Feedback<object>, IFeedback {
         public Feedback SetStatus(bool status) {
             Status = status;
             return this;
@@ -27,17 +24,26 @@ namespace Haley.Models {
             Message = string.Empty;
             return this;
         }
+        public Feedback() { }
+        public Feedback(bool status, string message, object result) :base(status,message,result) { }
+        public Feedback(bool status) : base(status, null,null) { }
+        public Feedback(bool status, string message) : base(status, message, null) { }
+    }
 
+    public class Feedback<T> : IFeedback<T> {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
         public override string ToString() {
             return $@"{Status} : {Message} -- {Result}";
         }
         public Feedback() { }
-        public Feedback(bool status, string message, object result) {
+        public Feedback(bool status, string message, T result) {
             Status = status;
             Message = message;
             Result = result;
         }
-        public Feedback(bool status) : this(status, null, null) { }
-        public Feedback(bool status, string message) : this(status, message, null) { }
+        public Feedback(bool status) : this(status, null) { }
+        public Feedback(bool status, string message) : this(status, message, default) { }
     }
 }
