c6d83daf33402ea149b2d90bc028c68e2f21cbb7
LingamHPZ4 <rmsmech@gmail.com>
2025-08-17 12:27:59 +0400

    Version Changes

---
+++ HaleyAbstractions/Helpers/Abstractions/IEmailData.cs
@@ -0,0 +1,12 @@
+﻿namespace Haley.Models {
+    public interface IEmailData {
+        string[] To { get; set; }
+        string[] CC { get; set; }
+        string[] BCC { get; set; }
+        string[] ReplyTo { get; set; }
+        string Subject { get; set; }
+        string Body { get; set; }
+        string From { get; set; }
+        bool IsHtml { get; set; } 
+    }
+}
