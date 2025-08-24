9860575dc7184093b661bb2d38f7563540653257
LingamHPZ4 <rmsmech@gmail.com>
2025-08-24 14:03:09 +0400

    all changes

--- HaleyAbstractions/Common/Abstractions/ICompositeObj.cs
+++ HaleyAbstractions/Common/Abstractions/ICompositeObj.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Haley.Abstractions {
     public interface ICompositeObj<T> : IIdentityBase where T : ICompositeObj<T> {
+        long ParentId { get; set; }
         List<T> Children { get; set; }
     }
 }
--- HaleyAbstractions/Common/Models/IdentityBase.cs
+++ HaleyAbstractions/Common/Models/IdentityBase.cs
@@ -1,12 +1,22 @@
 ﻿using Haley.Abstractions;
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Haley.Models
 {
     public class IdentityBase : IIdentityBase {
+        [JsonPropertyName("guid")]
+        [JsonPropertyOrder(3)]
         public Guid Guid { get; }
+        [JsonPropertyName("key")]
+        [JsonPropertyOrder(2)]
         public string Key { get; set; }
+        [JsonPropertyName("id")]
+        [JsonPropertyOrder(1)]
         public long Id { get; set; }
+        [JsonPropertyName("name")]
+        [JsonPropertyOrder(4)]
         public string Name { get; set; }
         public IdentityBase() : this(null) { }
         public IdentityBase(string key) {
