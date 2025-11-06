be30f21c5e54479df3f437ce25a9d7e6d8a15e37
LingamHPZ4 <rmsmech@gmail.com>
2025-11-06 15:57:04 +0400

    Fix issues

---
+++ HaleyAbstractions/Common/Models/AppMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haley.Models {
+    public class AppMode {
+        public bool EnvInDevelopment { get; set; }
+        public bool AppInDevelopment { get; set; }
+        public bool ThrowExceptions { get; set; }
+    }
+}
