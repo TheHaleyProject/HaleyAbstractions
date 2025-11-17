41038c44f7bbceb6544ac5b7e7537e7a1394fd64
LingamHPZ4 <rmsmech@gmail.com>
2025-11-17 16:01:52 +0400

    All Changes

---
+++ HaleyAbstractions/Common/Models/AppFlags.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haley.Models {
+    public class AppFlags {
+        public bool DevEnvironment { get; set; }
+        public bool Mock { get; set; } = true; //By default Mock is always true //We control few things like emails etc, and modify the settings accordingly. 
+        public bool ConsoleLog { get; set; }
+        public bool Debug { get; set; }
+        public bool ThrowExceptions { get; set; }
+
+        public override string ToString() {
+            return $@"Dev={DevEnvironment} | Mock={Mock} | ConsoleLog={ConsoleLog} | Debug={Debug} | ThrowExceptions={ThrowExceptions}";
+        }
+    }
+}
