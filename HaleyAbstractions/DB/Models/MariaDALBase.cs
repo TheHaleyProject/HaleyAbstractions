6a1f126ccdeef32003da2b02c55d2a119f9ebdbd
Quetzal <rmsmech@outlook.com>
2026-03-06 01:31:12 +0400

    All abstractions migrated..

---
+++ HaleyAbstractions/DB/Models/MariaDALBase.cs
@@ -0,0 +1,11 @@
+﻿using Haley.Abstractions;
+using System.Threading.Tasks;
+
+namespace Haley.Models {
+    public abstract class MariaDALBase {
+        protected readonly IDALUtilBase Db;
+        protected MariaDALBase(IDALUtilBase db) => Db = db;
+        protected async Task<bool> ExistsAsync(string existsSql, DbExecutionLoad load, params DbArg[] args)
+            => (await Db.ScalarAsync<int>(existsSql, load, args)) == 1;
+    }
+}
