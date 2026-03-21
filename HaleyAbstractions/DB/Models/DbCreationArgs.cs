6906a919aba8a1362a131d0941f75d6b6315fe65
Quetzal <rmsmech@outlook.com>
2026-03-22 03:44:38 +0400

    refactoring

--- HaleyAbstractions/DB/Models/DbCreationArgs.cs
+++ HaleyAbstractions/DB/Models/DbCreationArgs.cs
@@ -12,6 +12,7 @@ namespace Haley.Models
         public string DBName { get; set; }
         public string SQLPath { get; set; }
         public string SQLContent { get; set; }
+        public string? CloningAdapterKey { get; set; } //Sometimes, the adapter gateway might not have the kye.. in such cases.. we need to hceck if the cloningadpaterkey is present or not,, in this case, we can just duplicate the adapter and then just set new key and a new database name.. thats it.. Dbname and key are already present here.. if cloningadpater is present we just duplicate
         public Dictionary<string, string> VariablesToReplace { get; set; } = new Dictionary<string, string>();
         public Func<string ,string,string> ContentProcessor { get; set; }
         public DbCreationArgs(string adapter_key) { Key = adapter_key;}
